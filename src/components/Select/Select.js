37eab83def07e4fdd2467f479d7415e39114a712
donjoe <giumontanaro@gmail.com>
2019-01-16 21:00:03 +0100

    Create Select component and style

---
+++ src/components/Select/Select.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Select = (props) => {
+    const { value, optValues, onSelect, label, className } = props;
+    const options = optValues.map(val => (
+        <option key={val} value={val}>{val}</option>
+    ));
+
+    if (label) {
+        return (
+            <label className='controls-wrapper__label'>
+                {label}
+                <select className={className} value={value} onChange={onSelect}>
+                    {options}
+                </select>
+            </label>
+        );
+    } else {
+        return (
+            <select className={className} value={value} onChange={onSelect}>
+                {options}
+            </select>
+        );
+    }
+}
+
+export default Select;
