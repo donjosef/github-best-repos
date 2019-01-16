42bec5994fb998b5deb1c06b08a852b33ec6137f
donjoe <giumontanaro@gmail.com>
2019-01-16 21:00:27 +0100

    Import and uSe Select inside App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Select from './components/Select/Select';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,16 @@ class App extends Component {
         <header className='header'>
           <h1 className='header__title'>Most popular github repos</h1>
         </header>
+        <div className='controls-wrapper'>
+          <Select 
+            className='controls-wrapper__select'
+            label='Search' 
+            optValues={['all', 'javascript', 'java', 'python', 'ruby']}/>
+          <Select 
+            className='controls-wrapper__select'
+            label='for' 
+            optValues={['all', 'last 1 year', 'last 5 years']}/>
+        </div>
       </div>
     );
   }
