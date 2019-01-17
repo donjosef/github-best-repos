bd765dd73ce22af889510f7fdd8a0e48c68aefd4
donjoe <giumontanaro@gmail.com>
2019-01-17 15:19:05 +0100

    Create header of Hit with received props

--- src/components/Hits/Hit/Hit.js
+++ src/components/Hits/Hit/Hit.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStar)
+
 const Hit = (props) => {
-  return (
-    <li>Repo title</li>
-  )
+    const { name, url, avatar, stars, language,  } = props;
+    return (
+        <li className='hit'>
+            <div className='hit__header'>
+                <div className='hit__avatar'>
+                    <img src={avatar} />
+                </div>
+                <h2 className='hit__title'><a href={url}>{name}</a></h2>
+                <div className='hit__language'>
+                    <p>{language}</p>
+                </div>
+                <div className='hit__stars'>
+                    <FontAwesomeIcon icon="star" size='xs' color='#FBBC05' />
+                    <p>{stars}</p>
+                </div>
+            </div>
+        </li>
+    )
 }
 
 export default Hit;
