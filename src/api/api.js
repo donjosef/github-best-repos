05d8ca0c64940b13270935e4e19649cab601b24b
donjoe <giumontanaro@gmail.com>
2019-01-17 19:01:04 +0100

    Use parse link to retrieve the number of total pages of github response

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import parseLink from 'parse-link-header';
 
 export async function getRepos(language, date, page = 1) {
     const res = await axios.get(`https://api.github.com/search/repositories?q=stars:>1+language:${language}+created:>${date}&sort=stars&order=desc&per_page=50&page=${page}`);
+    const pageCount = parseInt(parseLink(res.headers.link).last.page, 10);
     
     return {
-        hits: res.data.items
+        hits: res.data.items,
+        pageCount
     }
 
 }
