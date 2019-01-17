e44e8ac0d53263c9acea512a992080df7f8e9757
donjoe <giumontanaro@gmail.com>
2019-01-17 15:17:39 +0100

    Pass props down to Hit

--- src/components/Hits/Hits.js
+++ src/components/Hits/Hits.js
@@ -3,11 +3,20 @@ import Hit from './Hit/Hit';
 
 const Hits = (props) => {
     const { hits } = props;
-  return (
-    <ul className='hits'>
-       {hits.map(hit => <Hit key={hit.id}/>)} 
-    </ul>
-  )
+    return (
+        <ul className='hits'>
+            {hits.map(hit => (
+                <Hit 
+                    key={hit.id}
+                    name={hit.name}
+                    url={hit.html_url}
+                    avatar={hit.owner.avatar_url}
+                    stars={hit.stargazers_count}
+                    language={hit.language}
+                     />
+            ))}
+        </ul>
+    );
 }
 
 export default Hits;
