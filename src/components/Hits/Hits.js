38eef87974b24778a16eb6bd973caa15ee84c250
donjoe <giumontanaro@gmail.com>
2019-01-19 16:25:55 +0100

    Navigate through pages with history prop, getting new repos for current page

--- src/components/Hits/Hits.js
+++ src/components/Hits/Hits.js
@@ -31,6 +31,32 @@ class Hits extends React.Component {
             .catch(err => console.log(err))
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        /*Since the path changed, get repos for the new page*/
+        if (prevProps.location.pathname !== this.props.location.pathname) {
+            let page;
+            const regEx = /\d+/;
+            const { language, date } = this.props;
+
+            if(this.props.location.pathname === '/') {
+                page = 1;
+            } else {
+                page = this.props.location.pathname.match(regEx)[0]; //extract the number of current page from pathname
+            }
+            
+            getRepos(language, date, page)
+                .then(data => {
+                    this.setState({ hits: data.hits });
+                })
+                .catch(err => console.log(err))
+        }
+    }
+
+    changePageHandler = (data) => {
+        const page = data.selected + 1; //data.selected is 0 based
+        this.props.history.push('/page' + page); //navigate programmatically through pages, changing the path
+    }
+
     render() {
         const { hits } = this.state;
         return (
@@ -55,6 +81,7 @@ class Hits extends React.Component {
                     pageCount={this.state.pageCount}
                     pageRangeDisplayed={3}
                     marginPagesDisplayed={2}
+                    onPageChange={this.changePageHandler}
                     containerClassName='paginate-wrapper'
                     pageLinkClassName='paginate-link'
                     pageClassName='paginate-li'
