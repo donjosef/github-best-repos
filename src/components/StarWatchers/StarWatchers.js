d969a6bcc941d559124fb09eb0233047f3f08ce8
donjoe <giumontanaro@gmail.com>
2019-01-20 18:20:21 +0100

    Use Paginate for StarWatchers component

--- src/components/StarWatchers/StarWatchers.js
+++ src/components/StarWatchers/StarWatchers.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import Watcher from './Watcher/Watcher';
+
+import Paginate from 'react-paginate';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+
 import './StarWatchers.css';
 import { getWatchers } from '../../api/api';
+
+library.add(faChevronLeft, faChevronRight);
 
 class StarWatchers extends Component {
     state = {
@@ -29,6 +39,21 @@ class StarWatchers extends Component {
                 <ul className='watchers'>
                     {watchers}
                 </ul>
+
+                <Paginate
+                        pageCount={this.state.pageCount}
+                        pageRangeDisplayed={3}
+                        marginPagesDisplayed={2}
+                        disableInitialCallback
+                        containerClassName='paginate-wrapper'
+                        pageLinkClassName='paginate-link'
+                        pageClassName='paginate-li'
+                        previousClassName='paginate-li'
+                        nextClassName='paginate-li'
+                        previousLabel={<FontAwesomeIcon icon='chevron-left' />}
+                        nextLabel={<FontAwesomeIcon icon='chevron-right' />}
+                        activeClassName='active-link'
+                    />
             </div>
         )
     }
