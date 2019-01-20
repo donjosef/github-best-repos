f4bb80cb8754f08fc673222b79c53b74b3ecf384
donjoe <giumontanaro@gmail.com>
2019-01-20 18:14:19 +0100

    Assign url to href for that particular watcher

--- src/components/StarWatchers/Watcher/Watcher.js
+++ src/components/StarWatchers/Watcher/Watcher.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Watcher = (props) => {
-    const { avatar, name} = props;
+    const { avatar, name, url} = props;
     return (
         <li className='watcher'>
             <div className='watcher__avatar'>
                 <img src={avatar} />
             </div>
-            <h3>{name} Test</h3>
+            <h3><a href={url} >{name}</a></h3>
         </li>
     )
 }
