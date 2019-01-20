fedd41848a232b6047d5c71378116b58ed2002ac
donjoe <giumontanaro@gmail.com>
2019-01-20 18:13:41 +0100

    When component mounts make a get req to github api for watchers

--- src/components/StarWatchers/StarWatchers.js
+++ src/components/StarWatchers/StarWatchers.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import Watcher from './Watcher/Watcher';
 import './StarWatchers.css';
+import { getWatchers } from '../../api/api';
 
 class StarWatchers extends Component {
     state = {
         watchers: []
     }
 
+    componentDidMount() {
+        const { owner, repo } = this.props.match.params;
+        getWatchers(owner, repo)
+            .then(data => {
+                this.setState({ watchers: data.watchers })
+            })
+    }
     render() {
-        const watchers = this.state.watchers.map(watcher => <Watcher avatar={watcher.avatar_url} name={watcher.login}/>);
+        const watchers = this.state.watchers.map(watcher => {
+            return <Watcher 
+                        key={watcher.id} 
+                        avatar={watcher.avatar_url} 
+                        name={watcher.login}
+                        url={watcher.html_url} />
+        });
+
         return (
             <div>
                 <ul className='watchers'>
