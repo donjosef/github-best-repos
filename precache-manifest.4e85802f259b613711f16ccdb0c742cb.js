f1f40b8ef27f7e1ae34fdc922d652d1f0f1f26dd
donjoe <giumontanaro@gmail.com>
2019-01-22 16:39:15 +0100

    Updates

---
+++ precache-manifest.4e85802f259b613711f16ccdb0c742cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18676e383f6f41b0b43c",
+    "url": "/github-best-repos/static/js/runtime~main.18676e38.js"
+  },
+  {
+    "revision": "39aefccaf5d82addc229",
+    "url": "/github-best-repos/static/js/main.39aefcca.chunk.js"
+  },
+  {
+    "revision": "f9ea71d94713c20c1926",
+    "url": "/github-best-repos/static/js/1.f9ea71d9.chunk.js"
+  },
+  {
+    "revision": "39aefccaf5d82addc229",
+    "url": "/github-best-repos/static/css/main.a21d14e2.chunk.css"
+  },
+  {
+    "revision": "a70f4856c541c533e797420f84b10415",
+    "url": "/github-best-repos/index.html"
+  }
+];
