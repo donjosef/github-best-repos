28aa2170fde428f5768fe8fd4c3317e5f61631c0
donjoe <giumontanaro@gmail.com>
2019-02-09 16:18:01 +0100

    Use WithPaginate with Hits

--- src/components/Hits/Hits.js
+++ src/components/Hits/Hits.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import Hit from './Hit/Hit';
-import Paginate from 'react-paginate';
+import WithPaginate from '../../hoc/WithPaginate/WithPaginate';
 import LoadingBar from '../LoadingBar/LoadingBar';
 
 import { getRepos } from '../../api/api';
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faChevronLeft, faChevronRight);
 
 class Hits extends React.Component {
 
@@ -96,27 +89,16 @@ class Hits extends React.Component {
         }
     }
 
-    changePageHandler = (data) => {
-        const page = data.selected + 1; //data.selected is 0 based
-        this.props.history.push('/page' + page); //navigate programmatically through pages, changing the path
-    }
-
     render() {
         const { hits } = this.state;
 
-        /*Retrieve currentPage from pathaname not from state, because on reload the state is reset*/
-        const pathname = this.props.location.pathname;
-        const regex = /\d+/;
-        let currentPage;
-        if (pathname === '/') {
-            currentPage = 1;
-        } else {
-            currentPage = parseInt(pathname.match(regex)[0], 10);
-        }
-
         if (!this.state.loading && this.state.percentage >= 100) {
             return (
-                <div>
+                <WithPaginate pageCount={this.state.pageCount} router={{
+                    history: this.props.history,
+                    match: this.props.match,
+                    location: this.props.location
+                    }}>
                     <ul className='hits'>
                         {hits.map(hit => {
                             /*Remove dummy repo named eeeeee from the results*/
@@ -140,25 +122,8 @@ class Hits extends React.Component {
                             }
                         })}
                     </ul>
-
-                    <Paginate
-                        pageCount={this.state.pageCount}
-                        pageRangeDisplayed={3}
-                        marginPagesDisplayed={2}
-                        forcePage={currentPage - 1}
-                        onPageChange={this.changePageHandler}
-                        disableInitialCallback
-                        containerClassName='paginate-wrapper'
-                        pageLinkClassName='paginate-link'
-                        pageClassName='paginate-li'
-                        previousClassName='paginate-li'
-                        nextClassName='paginate-li'
-                        previousLabel={<FontAwesomeIcon icon='chevron-left' />}
-                        nextLabel={<FontAwesomeIcon icon='chevron-right' />}
-                        activeClassName='active-link'
-                    />
-                </div>
-            );
+                </WithPaginate>
+            )    
         } else {
             if (!this.state.error) {
                 return <LoadingBar percentage={this.state.percentage} />
