6a96de902236011203a6cb8b391978b278f8762e
donjoe <giumontanaro@gmail.com>
2019-02-10 11:35:58 +0100

    Add parsed html as innerHtml of ReadMePage component

--- src/components/ReadMePage/ReadMePage.js
+++ src/components/ReadMePage/ReadMePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getReadMe } from '../../api/api';
+import { parseHtml, walkTheNode } from '../../utilities/utilities';
 const marked = require('marked');
 
 class ReadMePage extends Component {
@@ -18,8 +19,27 @@ class ReadMePage extends Component {
     }
 
     render() {
-        return <div dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}></div>
+        /*
+            * marked return a string representing the html
+            * parse that string, so one can walks recursively the dom
+            * reset alignment of text and remove all images from original markdown, to avoid layout problems
+            * set as innerHtml of this component
+        */
+        const htmlInString = marked(this.state.markdown);
+        const parsed = parseHtml(htmlInString);
+        walkTheNode(parsed, (node) => {
+            if(node.nodeType === 1) {
+                if(node.getAttribute('align')) {
+                    node.removeAttribute('align');
+                }
+                if(node.nodeName === 'IMG') {
+                    node.remove();
+                }
+            }
+        });
+        return <div className="readme-page" dangerouslySetInnerHTML={{__html: parsed.innerHTML}}></div>
     }
 }
+
 
 export default ReadMePage;
