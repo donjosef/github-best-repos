70d4ffb052c7cbe6e56fbc261c19a61d18c5280a
donjoe <giumontanaro@gmail.com>
2019-02-11 17:04:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/github-best-repos/precache-manifest.1034d2b9ce60878cf584c430a95e5165.js"
+  "/github-best-repos/precache-manifest.f7dc941238549713de6f9554d043eca4.js"
 );
 
 workbox.clientsClaim();
