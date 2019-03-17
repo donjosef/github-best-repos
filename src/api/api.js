b207658c52e6fd074256484b77db2148c479fa27
donjoe <giumontanaro@gmail.com>
2019-03-17 22:37:28 +0100

    Create a generic getData function that makes the get requests based on type of data

--- src/api/api.js
+++ src/api/api.js
@@ -5,9 +5,17 @@ function isLastPage(headersLink) {
     return headersLink.last ? false : true;
 }
 
-export async function getRepos(language, date, page = 1) {
-    const res = await axios.get(`https://api.github.com/search/repositories?q=stars:>1+language:${language}+created:>${date}&sort=stars&order=desc&per_page=40&page=${page}`);
-    
+export async function getData(type, searchParams, page = 1) {
+    let res;
+
+    if(type === 'repos') {
+        res = await axios.get(`https://api.github.com/search/repositories?q=stars:>1+language:${searchParams.language}+created:>${searchParams.date}&sort=stars&order=desc&per_page=40&page=${page}`);
+    }
+
+    if(type === 'starwatchers') {
+        res = await axios.get(`https://api.github.com/repos/${searchParams.owner}/${searchParams.repo}/stargazers?per_page=40&page=${page}`);
+    }
+
     let pageCount;
     if (isLastPage(parseLink(res.headers.link))) {
         pageCount = parseInt(parseLink(res.headers.link).prev.page, 10) + 1;
@@ -15,15 +23,14 @@ export async function getRepos(language, date, page = 1) {
         pageCount = parseInt(parseLink(res.headers.link).last.page, 10);
     }
 
-    return {
-        hits: res.data.items,
-        pageCount
-    }
+    /*return the object representing the response for repos or starwatchers. This response will be handled by FetchData component to set the state*/
+    return type === 'repos' ? {data: res.data.items, pageCount } : {data: res.data, pageCount}
+    
 }
 
 export async function getReposDynamically(query) {
     const res = await axios.get(`https://api.github.com/search/repositories?q=${query}+in:name+stars:>1000&per_page=10`);
-    
+
     return {
         results: res.data.items
     };
@@ -32,21 +39,4 @@ export async function getReposDynamically(query) {
 export async function getReadMe(owner, repo) {
     const res = await axios.get(`https://raw.githubusercontent.com/${owner}/${repo}/master/README.md`);
     return res.data;
-}
-
-
-export async function getWatchers(owner, repo, page = 1) {
-    const res = await axios.get(`https://api.github.com/repos/${owner}/${repo}/stargazers?per_page=40&page=${page}`);
-
-    let pageCount;
-    if (isLastPage(parseLink(res.headers.link))) {
-        pageCount = parseInt(parseLink(res.headers.link).prev.page, 10) + 1;
-    } else {
-        pageCount = parseInt(parseLink(res.headers.link).last.page, 10);
-    }
-
-    return {
-        watchers: res.data,
-        pageCount
-    }
 }
