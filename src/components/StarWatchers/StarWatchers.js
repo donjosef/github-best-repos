43f3610779bb83d30ceec3ee475476b3d4a87e3f
donjoe <giumontanaro@gmail.com>
2019-03-17 22:40:26 +0100

    Modified StarWatchers. Now uses FetchData to render dinamically its content

--- src/components/StarWatchers/StarWatchers.js
+++ src/components/StarWatchers/StarWatchers.js
@@ -1,90 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Watcher from './Watcher/Watcher';
-
+import FetchData from '../../containers/FetchData/FetchData';
 import WithPaginate from '../../hoc/WithPaginate/WithPaginate';
 import './StarWatchers.css';
-import { getWatchers } from '../../api/api';
 
-class StarWatchers extends Component {
-    state = {
-        watchers: [],
-        pageCount: 0,
-        error: null
+/*
+IMPORTANT: 
+displayWatchers gets invoked by render of FetchData. 
+FetchData passes its own state and props (its logic) to this function
+This function render jsx (specifically StarWatchers component)
+this component is concerned with presentation only just like Hits
+*/
+function displayWatchers(props) {
+    const watchers = props.data.map(watcher => {
+        return <Watcher
+            key={watcher.id}
+            avatar={watcher.avatar_url}
+            name={watcher.login}
+            url={watcher.html_url} />
+    });
+
+    let output =
+        <WithPaginate
+            pageCount={props.pageCount}
+            router={{
+                history: props.history,
+                match: props.match,
+                location: props.location
+            }}>
+            <ul className='watchers'>{watchers}</ul>
+        </WithPaginate>
+
+    if (props.error) {
+        output = <h1>{props.error}</h1>
     }
 
-    componentDidMount() {
-        const { owner, repo } = this.props.match.params;
-        const pathname = this.props.location.pathname;
-        if (pathname.endsWith('/starwatchers') || pathname.endsWith('/starwatchers/1')) {
-            getWatchers(owner, repo)
-                .then(data => {
-                    this.setState({
-                        watchers: data.watchers,
-                        pageCount: data.pageCount
-                    });
-                })
-                .catch(err => this.setState({ error: err.message }))
-        } else {
-            const regEx = /\d+/;
-            const currentPage = pathname.match(regEx)[0];
+    return output;
+}
 
-            getWatchers(owner, repo, currentPage)
-                .then(data => {
-                    this.setState({
-                        watchers: data.watchers,
-                        pageCount: data.pageCount
-                    });
-                })
-                .catch(err => this.setState({ error: err.message }))
-        }
-    }
+function StarWatchers(props) {
+    const { history, match, location } = props;
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.location.pathname !== this.props.location.pathname) {
-            let page;
-            const regEx = /\d+/;
-            const { owner, repo } = this.props.match.params;
-
-            if (!this.props.location.pathname.match(regEx)) {
-                page = 1;
-            } else {
-                page = this.props.location.pathname.match(regEx)[0]; //extract the number of current page from pathname
-            }
-
-            getWatchers(owner, repo, page)
-                .then(data => {
-                    this.setState({ watchers: data.watchers });
-                })
-
-        }
-    }
-
-    render() {
-        const watchers = this.state.watchers.map(watcher => {
-            return <Watcher
-                key={watcher.id}
-                avatar={watcher.avatar_url}
-                name={watcher.login}
-                url={watcher.html_url} />
-        });
-
-        let output =
-            <WithPaginate
-                pageCount={this.state.pageCount}
-                router={{
-                    history: this.props.history,
-                    match: this.props.match,
-                    location: this.props.location
-                }}>
-                <ul className='watchers'>{watchers}</ul>
-            </WithPaginate>
-
-        if (this.state.error) {
-            output = <h1>{this.state.error}</h1>
-        }
-
-        return output;
-    }
+    return (
+        <FetchData
+            type="starwatchers"
+            location={location}
+            history={history}
+            match={match}
+            searchParams={{owner: match.params.owner, repo: match.params.repo}}
+            render={displayWatchers}
+        />
+    )
 }
 
 export default StarWatchers;
