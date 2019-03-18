81646638ffbbfb3038bc9db5fa84699b89b479d3
donjoe <giumontanaro@gmail.com>
2019-03-18 17:40:28 +0100

    Implement loader on FakeHit

--- src/components/Hits/FakeHit/FakeHit.js
+++ src/components/Hits/FakeHit/FakeHit.js
@@ -4,17 +4,19 @@ function FakeHit(props) {
     return (
         <li className="hit">
             <div className='fake-hit__header'>
-                <div className='fake-avatar'></div>
+                <div className="fake-hit__loader">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
                 <div className='fake-title'></div>
-                <div className='fake-language'></div>
-                <div className='fake-stars'></div>
             </div>
 
             <div className='fake-hit__body'>
-               <div className='fake-type'></div>
-               <div className='fake-description'></div>
-               <div className='fake-creationDate'></div>
-               <div className='fake-updateDate'></div>
+                <div className='fake-type'></div>
+                <div className='fake-description'></div>
+                <div className='fake-creationDate'></div>
+                <div className='fake-updateDate'></div>
             </div>
         </li>
     )
